bb7cf2c72fc435395e5a97ec53314529a9427b3a
Mason Matlock <mwilliam09@gmail.com>
2016-04-13 16:46:58 -0500

    More Many to Many setup

---
+++ src/CatanLayoutGenerator/Models/UserBoards.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatanLayoutGenerator.Models {
+    public class UserBoards {
+        public int UserId { get; set; }
+        public int BoardLayoutId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public BoardLayout BoardLayout { get; set; }
+    }
+}
