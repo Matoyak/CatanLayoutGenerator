5177e1998693324a85679b4db3a147506ba87b21
Mason Matlock <mwilliam09@gmail.com>
2016-04-13 16:46:12 -0500

    Added basic ManyToMany Scaffolding

--- src/CatanLayoutGenerator/Models/BoardLayout.cs
+++ src/CatanLayoutGenerator/Models/BoardLayout.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 namespace CatanLayoutGenerator.Models {
     public class BoardLayout {
         public int Id { get; set; }
-        public string Layout { get; set; } //serialized Json of 19 hexs.
+        public string Layout { get; set; } //serialized Json of 19 hexs. (Comes from Board).
         public string LayoutImg { get; set; } //the initial idea to get project working.
+
+        public ICollection<UserBoards> UserBoards { get; set; }
     }
 }
